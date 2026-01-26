4958ece5494a1e0165ebfe6b1ed540cbb2233d72
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-26 10:59:47 +0100

    feat(service): Add GetUserByIdAsync and GetUserByEmailAsync in IUserService

--- PanMedix/Services/Interfaces/IUserService.cs
+++ PanMedix/Services/Interfaces/IUserService.cs
@@ -5,5 +5,7 @@ namespace PanMedix.Services.Interfaces;
 
 public interface IUserService
 {
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<User?> GetUserByIdAsync(string id);
     Task<User> AddUserAsync(User user, UserRole role, string? password);
 }
