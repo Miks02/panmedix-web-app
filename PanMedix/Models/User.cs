5fe5d4c887f151b00548b4384cda383684d3861c
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-01-28 20:08:52 +0100

    feat(dto): Add CreatedAt property in user model and commit migrations

--- PanMedix/Models/User.cs
+++ PanMedix/Models/User.cs
@@ -19,6 +19,8 @@ namespace PanMedix.Models
         public User? Guardian { get; set; }
 
         public bool IsGuardianApproved { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public ICollection<User> Patients { get; set; } = new List<User>();
         public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
